01a4ce362819ccd7ecaa865567ec46d0949b19b4
cuixiaorui <cui_xiaorui@126.com>
2021-07-29 18:04:47 +0800

    add cypress component unit test

---
+++ src/components/Button.cy.js
@@ -0,0 +1,9 @@
+import { mount } from "@cypress/vue";
+import Button from "./Button.vue";
+describe("Button.vue", () => {
+  it("第一个测试", () => {
+    // mount(Button);
+    console.log("1213");
+    console.log(Cypress.vue);
+  });
+});

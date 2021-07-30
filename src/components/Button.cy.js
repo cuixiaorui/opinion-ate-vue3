c207bce8488e43c1680b1b226b252fb901cf1d70
cuixiaorui <cui_xiaorui@126.com>
2021-07-30 16:56:29 +0800

    clear code

--- src/components/Button.cy.js
+++
@@ -1,9 +0,0 @@
-import { mount } from "@cypress/vue";
-import Button from "./Button.vue";
-describe("Button.vue", () => {
-  it("第一个测试", () => {
-    // mount(Button);
-    console.log("1213");
-    console.log(Cypress.vue);
-  });
-});

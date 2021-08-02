1b863f4822fbbf81c68a2e20a434859a85a1a78c
cuixiaorui <cui_xiaorui@126.com>
2021-08-02 16:09:40 +0800

    refactor

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,8 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
+import "cypress-jest-adapter";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
--- src/components/NewMessageForm.cy.js
+++ src/components/NewMessageForm.cy.js
@@ -7,19 +7,13 @@ describe("NewMessageForm", () => {
 
     // 点击 send click
     // when
-    // TODO
-    // 1.看看有没有更好的获取元素的方式 ， data-test 不应该是优先级最高的
-    // 	这个应该去看看 cypress 的匹配器的使用指导
-    // 2. 看看如何在 cypress 中使用 jest 的匹配器
-    //  chai 的匹配器 api 不是太熟悉
     cy.get("[data-test='messageText']").type("New message");
     cy.contains("send").click();
     cy.get("[data-test='messageText']").should("have.value", "");
   });
 
   it('emits the "send" event', () => {
-    expect(Cypress.vueWrapper.emitted("send")[0]).to.deep.equal([
-      "New message",
-    ]);
+    // 使用 jest 风格的匹配器 api
+    expect(Cypress.vueWrapper.emitted("send")[0]).toEqual(["New message"]);
   });
 });
